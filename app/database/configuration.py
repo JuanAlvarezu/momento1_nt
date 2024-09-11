012365fa7511033c1dac8d4c937837a92791fb0a
Juan Camilo Alvarezu <JuanAlvarez.ContactoPersonal@gmail.com>
2024-09-10 19:36:57 -0500

    Se crea repositorio del back momento1

---
+++ app/database/configuration.py
@@ -0,0 +1,7 @@
+dataBaseName = "momento1_nt"
+userName = 'root'
+userpassword = ''
+connectionPort = 3306
+server = 'localhost'
+
+databaseConnedction = f'mysql+mysqlconnectorpython://{userName}:{userpassword}@{server}:{connectionPort}/{dataBaseName}'
